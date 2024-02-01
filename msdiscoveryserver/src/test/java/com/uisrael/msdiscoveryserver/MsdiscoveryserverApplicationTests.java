4ea4bb050a5a48a8f78e64c3b4bf48777cc46fac
DanielGuachamin022 <guillermodaniel.guachamin@gmail.com>
2024-01-31 20:28:15 -0500

    creacion de proyecto

---
+++ msdiscoveryserver/src/test/java/com/uisrael/msdiscoveryserver/MsdiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.msdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsdiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
